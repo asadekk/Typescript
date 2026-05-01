414211a517f48c3b24274877c67962ae9a85aa47
asadekk <asadesd21@gmail.com>
2026-05-01 22:12:55 +0500

    Typescript topshiriq

---
+++ main.js
@@ -0,0 +1,7 @@
+"use strict";
+let message = "Salom, TypeScript!";
+let year = 2026;
+function greet(name) {
+    return "Xush kelibsiz, " + name;
+}
+console.log(greet(message));
---
+++ main.ts
@@ -0,0 +1,12 @@
+// let message: string = "Salom, TypeScript!";
+// let year: number = 2026;
+
+// function greet(name: string) {
+//     return "Xush kelibsiz, " + name;
+// }
+
+// console.log(greet(message));
+
+let mashina:string = 'MERS'
+let years:number = 13
+console.log(`moshina dodasi${mashina}`);

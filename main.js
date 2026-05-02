33a2194c03fc24dba734eb55e41932e7f69d84ff
asadekk <asadesd21@gmail.com>
2026-05-02 13:09:52 +0500

    Basic Type

--- main.js
+++ main.js
@@ -1,7 +1,22 @@
 "use strict";
-let message = "Salom, TypeScript!";
-let year = 2026;
-function greet(name) {
-    return "Xush kelibsiz, " + name;
+// let message: string = "Salom, TypeScript!";
+// let year: number = 2026;
+// function greet(name: string) {
+//     return "Xush kelibsiz, " + name;
+// }
+// console.log(greet(message));
+let mashina = 'MERS';
+let years = 13;
+let a;
+let m = true;
+let n = 123n;
+let o;
+let any = 123;
+let unknow = '123';
+function sum(name, tel) {
+    return name;
 }
-console.log(greet(message));
+console.log(sum('ASTA', 15));
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+let user;
+user = ['ki', 12];
--- main.ts
+++ main.ts
@@ -9,4 +9,22 @@
 
 let mashina:string = 'MERS'
 let years:number = 13
-console.log(`moshina dodasi${mashina}`);
+let a:undefined
+let m:boolean= true
+let n:bigint = 123n
+let o:null 
+
+
+let any:any = 123
+let unknow:unknown = '123'
+
+function sum(name:string,tel:number):string {
+    return name 
+}
+
+console.log(sum('ASTA',15));
+
+let arr:number[] = [1,2,3,4,5,6,7,8,9]
+let user:[string,number]
+user = ['ki',12]
+

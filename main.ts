69e057d5880727a4025f28c4c0763f31f23a8036
asadekk <asadesd21@gmail.com>
2026-05-03 16:48:10 +0500

    Obyekt va funksiya vaadvanced types

--- main.ts
+++ main.ts
@@ -1,30 +1,75 @@
-// let message: string = "Salom, TypeScript!";
-// let year: number = 2026;
+// // let message: string = "Salom, TypeScript!";
+// // let year: number = 2026;
 
-// function greet(name: string) {
-//     return "Xush kelibsiz, " + name;
+// // function greet(name: string) {
+// //     return "Xush kelibsiz, " + name;
+// // }
+
+// // console.log(greet(message));
+
+// let mashina:string = 'MERS'
+// let years:number = 13
+// let a:undefined
+// let m:boolean= true
+// let n:bigint = 123n
+// let o:null 
+
+
+// let any:any = 123
+// let unknow:unknown = '123'
+
+// function sum(name:string,tel:number):string {
+//     return name 
 // }
 
-// console.log(greet(message));
+// console.log(sum('ASTA',15));
 
-let mashina:string = 'MERS'
-let years:number = 13
-let a:undefined
-let m:boolean= true
-let n:bigint = 123n
-let o:null 
+// let arr:number[] = [1,2,3,4,5,6,7,8,9]
+// let user:[string,number]
+// user = ['ki',12]
+
+// let users:{id:number,name:string},[]=[
+//     {id: 1, name: 'Kirito'},
+//     {id: 2, name: 'Luffy'},
+//     {id: 3, name: 'Tanjito'},
+// ]
 
 
-let any:any = 123
-let unknow:unknown = '123'
 
-function sum(name:string,tel:number):string {
-    return name 
+// const lenovo:laptop={
+//     name:'Lenovo ideapad slim 3',
+// }
+// console.log(lenovo);
+
+// type victus = string | number;
+
+// let loptopBest: victus = 22
+
+// console.log(loptopBest);
+// interface laptop{
+//     name:string,
+// }
+// interface gaming {
+//     cpu:string;
+// }
+// type laptopB = laptop & gaming
+
+// let laptopGaming:laptopB = {
+//     name:'Lenovo legion 7 pro',
+//     cpu:"RTX 5090"
+// }
+// console.log(laptopGaming);
+
+// let arrQ:(string | number)[] = []
+// arrQ.push('asta')
+// arrQ.push(123)
+// console.log(arrQ);
+
+
+
+enum Colors {
+    red = 1,
+    green = 'uzbekistan',
+    blue = 3
 }
-
-console.log(sum('ASTA',15));
-
-let arr:number[] = [1,2,3,4,5,6,7,8,9]
-let user:[string,number]
-user = ['ki',12]
-
+console.log(Colors);
